7df4d2d25099916d13e2942261c5970dc0fb870d
Maziyar <Maziyarkolagar@gmail.com>
2025-02-18 22:40:53 +0330

    Add files via upload

---
+++ session-4/MyProject/myapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+from datetime import datetime
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+def book_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        published_date = request.POST.get('published_date')
+        Book.objects.create(title=title, author=author, published_date=published_date)
+        return redirect('book_list')
+    return render(request, 'book_create.html')
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.published_date = request.POST.get('published_date')
+        book.save()
+        return redirect('book_list')
+    return render(request, 'book_update.html', {'book': book})
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'book_confirm_delete.html', {'book': book})
