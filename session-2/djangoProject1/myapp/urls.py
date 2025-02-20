291ce7c855764ec7740b6a7d3210c073cdb10eee
Maziyar <Maziyarkolagar@gmail.com>
2025-02-20 18:20:58 +0330

    Update urls.py

--- session-2/djangoProject1/myapp/urls.py
+++ session-2/djangoProject1/myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+#This means we wanna use the views of this dierctory and give them addresses
 from . import views
 
 urlpatterns = [
+ #we we put '' empty it means its our main page
  path('', views.interoduce, name='name'),
  path('about/', views.about, name='about'),
 ]
