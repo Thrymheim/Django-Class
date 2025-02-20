0e5153b1e03a63ebf06847d2a9d2d6b246eec5d0
Maziyar <Maziyarkolagar@gmail.com>
2025-02-20 18:20:09 +0330

    Update views.py

--- session-2/djangoProject1/myapp/views.py
+++ session-2/djangoProject1/myapp/views.py
@@ -1,6 +1,9 @@
+#When we wanna use HTML contact we use render
 from django.shortcuts import render
 
+#Simple day for saying our name
 def interoduce(request):
+    #When we wanna use our contact in the HTML page we use context
     context = {
         'name':'maziyar',
     }
