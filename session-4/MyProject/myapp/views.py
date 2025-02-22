5fc5d57f0cb152c367e8de7de5bf301655ef5ae3
Maziyar <Maziyarkolagar@gmail.com>
2025-02-22 13:23:02 +0330

    Update views.py

--- session-4/MyProject/myapp/views.py
+++ session-4/MyProject/myapp/views.py
@@ -14,16 +14,19 @@ def book_create(request):
         title = request.POST.get('title')
         author = request.POST.get('author')
         published_date = request.POST.get('published_date')
+        #Get what we enter in HTML and put them in these variables, then pass them to model
         Book.objects.create(title=title, author=author, published_date=published_date)
         return redirect('book_list')
     return render(request, 'book_create.html')
 
 def book_update(request, pk):
+    #Find the book we want to update by pk since they are uique for each book
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         book.title = request.POST.get('title')
         book.author = request.POST.get('author')
         book.published_date = request.POST.get('published_date')
+        #Save things we did
         book.save()
         return redirect('book_list')
     return render(request, 'book_update.html', {'book': book})
